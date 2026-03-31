859f7b649001681d76e71210de79ce66b0da5969
per777duck <demyan_permyakov@bk.ru>
2026-04-01 01:09:19 +0700

    Updated map, map_mask and map_loader

--- app/src/main/java/com/example/myapplication/data/map/MapMatrixLoader.kt
+++ app/src/main/java/com/example/myapplication/data/map/MapMatrixLoader.kt
@@ -4,18 +4,20 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
+import kotlin.math.ceil
+import androidx.core.graphics.get
 
-data class MapData(val matrix: Array<IntArray>, val width: Int, val length: Int)
+class MapData(val matrix: Array<IntArray>, val width: Int, val length: Int)
 {
     fun isAvailable(x:Int, y:Int): Boolean
     {
-        if (x < 0 || x >= width || y < 0 || y >= length) return false
+        if (x !in 0..<width || y !in 0..<length) return false
         return matrix[y][x] == 1
     }
 
     fun getCell(x:Int, y:Int): Int
     {
-        if (x < 0 || x >= width || y < 0 || y >= length) return 0
+        if (x !in 0..<width || y !in 0..<length) return 0
         return matrix[y][x]
     }
 }
@@ -26,21 +28,35 @@ class MapMatrixLoader(private val context: Context)
     fun mapToMatrix(pictureId: Int): MapData
     {
         val picture: Bitmap = BitmapFactory.decodeResource(context.resources, pictureId)
-        val matrixSize = 100
-        val matrix = Array(matrixSize){ IntArray(matrixSize) }
 
-        for (y in 0 until matrixSize)
+        val cellSize = 8
+        val matrixWidth = ceil(picture.width.toDouble() / cellSize).toInt()
+        val matrixHeight = ceil(picture.height.toDouble() / cellSize).toInt()
+
+        val matrix = Array(matrixHeight){ IntArray(matrixWidth) }
+
+        for (y in 0 until matrixHeight)
         {
-            for (x in 0 until matrixSize)
+            for (x in 0 until matrixWidth)
             {
-                val pixel = picture.getPixel(x, y)
-                val brightness = Color.red(pixel)
-                matrix[y][x] = (if (brightness > 128) 1 else 0)
+                val centerX = x * cellSize + cellSize / 2
+                val centerY = y * cellSize + cellSize / 2
+
+                if (centerX < picture.width && centerY < picture.height){
+                    val pixel = picture[centerX, centerY]
+
+                    val red = Color.red(pixel)
+                    val green = Color.green(pixel)
+                    val blue = Color.blue(pixel)
+
+                    val brightness = (red + green + blue) / 3
+                    matrix[x][y] = if (brightness > 128) 1 else 0
+                }
             }
         }
 
         picture.recycle()
-        return MapData(matrix, matrixSize, matrixSize)
+        return MapData(matrix, matrixWidth, matrixHeight)
     }
 }
 
